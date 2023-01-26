5a5bba7ecfd4ac4ca37bfe8fdd2d53f2a05f962d
Coni Hinojosa <coni@Conis-MacBook-Pro.local>
2023-01-26 22:11:00 +0000

    created a function to get the max temperature for the next 5 days

--- assets/script.js
+++ assets/script.js
@@ -35,16 +35,26 @@ function fetchCity(cityName) {
 
 function fetchForecast(cityData) {
     let APIKey = "4a57a390d1ee8b328124d4af372fdaec";
-    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityData.coord.lat}&lon=${cityData.coord.lon}&appid=${APIKey}`;
+    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityData.name}&units=metric&appid=${APIKey}`;
+
+
 
     fetch(queryURL)
         .then(function (response) { return response.json(); })
         .then(function (forecastData) {
 
             function renderForecast() {
-                console.log(forecastData);
                 forecastSection.innerHTML =
-                    `<h5> 5-Day Forecast </h5>`;
+                    `<h5> 5-Day Forecast: </h5>`;
+
+                // for (let index = 0; index < 5; index++) {
+                //     let forecastTemp = forecastData.list[index].main.temp;
+                //     let forecastWind = forecastData.list[index].wind.speed;
+                //     console.log(forecastTemp);
+                // }
+
+                organizeDays(forecastData);
+                // console.log(forecastData);
             }
             renderForecast();
         });
@@ -57,3 +67,44 @@ document.getElementById("search-button").addEventListener("click", function (eve
 
     fetchCity(cityName);
 });
+
+
+function organizeDays(forecastData) {
+    let fiveDaysTemperatures = {};
+
+    for (let index = 0; index < forecastData.list.length; index++) {
+        let days = forecastData.list[index].dt_txt;
+        let formattedDays = moment(days).format("DD/MM/YYYY");
+        let temperature = forecastData.list[index].main.temp;
+
+        if (fiveDaysTemperatures[formattedDays] === undefined) {
+            fiveDaysTemperatures[formattedDays] = [temperature];
+        } else {
+            fiveDaysTemperatures[formattedDays].push(temperature);
+        }
+    }
+
+
+
+    function getMaxTemp(day) {
+
+        let maxTemp = fiveDaysTemperatures[day][0];
+        for (let index = 0; index < fiveDaysTemperatures[day].length; index++) {
+
+            if (fiveDaysTemperatures[day][index] > maxTemp) {
+                maxTemp = fiveDaysTemperatures[day][index];
+            }
+        }
+
+        console.log(maxTemp);
+    }
+
+    for (let index = 0; index < Object.keys(fiveDaysTemperatures).length; index++) {
+        let day = Object.keys(fiveDaysTemperatures)[index];
+        getMaxTemp(day);
+
+    }
+
+}
+
+
